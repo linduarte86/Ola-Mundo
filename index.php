b0a2f6eda5fc31c9d7e79b74412067b02f2852de
linduarte86 <linduarte86@gmail.com>
2023-01-30 22:27:57 -0300

    Adicionei o css

--- index.php
+++ index.php
@@ -5,12 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Olá Mundo</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <h1>
-        <?php
-            echo "Olá, Mundo!"; 
-        ?>
-    </h1>
+    <div class="center">
+        <h1 class="center">
+            <?php
+                echo "Olá, Mundo!"; 
+            ?>
+        </h1>
+    </div>
+    
 </body>
 </html>
